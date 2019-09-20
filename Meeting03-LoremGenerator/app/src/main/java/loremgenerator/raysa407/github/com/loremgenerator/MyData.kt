8ac54dce34949b79bf9218c0a5cc642ada369e5f
raysa407 <raysa407.goh@gmail.com>
2019-09-20 10:44:49 +0700

    MP_Projects

---
+++ Meeting03-LoremGenerator/app/src/main/java/loremgenerator/raysa407/github/com/loremgenerator/MyData.kt
@@ -0,0 +1,5 @@
+package loremgenerator.raysa407.github.com.loremgenerator
+
+import android.support.constraint.Placeholder
+
+data class  MyData(var placeholder: String = " ", var creator: String = " ")
